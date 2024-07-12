662f6756257348e05a888d9a07b5d2bc5a4d2287
David Reynolds <dgazeyreyn@gmail.com>
2024-07-12 19:06:41 +0000

    creating user fact table

---
+++ models/marts/fct_user_purchase_metrics.sql
@@ -0,0 +1,31 @@
+with responses as (
+
+    select * from {{ ref('stg_surveys__responses') }}
+
+),
+
+purchases_agg as (
+
+    select * from {{ ref('int_purchases_agg') }}
+
+),
+
+final as (
+
+select
+    responses.*,
+    purchases_agg.purchases,
+    purchases_agg.order_dates,
+    purchases_agg.total_purchase_price_per_unit,
+    purchases_agg.total_quantity,
+    purchases_agg.total_spend,
+    purchases_agg.products,
+    purchases_agg.categories
+from
+    responses
+left join
+    purchases_agg on responses.survey_responseid = purchases_agg.survey_responseid
+
+)
+
+select * from final
