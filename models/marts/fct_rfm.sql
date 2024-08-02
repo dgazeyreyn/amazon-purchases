2d2a681f5a14b67012aec49f6e138a76c36b36d6
David Reynolds <dgazeyreyn@gmail.com>
2024-08-02 16:26:13 +0000

    refactoring fct_rfm

--- models/marts/fct_rfm.sql
+++ models/marts/fct_rfm.sql
@@ -18,13 +18,35 @@ with
             *,
             ntile(5) over (order by recency desc) as recency_quintile,
             ntile(5) over (order by purchases) as frequency_quintile,
+            ntile(5) over (order by total_spend) as monetary_quintile
         from recency_days
     )
 select
-    concat(recency_quintile, ',', frequency_quintile) as rf_segment,
-    recency_quintile,
-    frequency_quintile,
-    sum(total_spend) as monetary_value,
-    count(*) as users
+    recency_quintile as quintile,
+    'recency' as metric,
+    count(*) as users,
+    min(recency) as minimum,
+    max(recency) as maximum,
+    avg(recency) as average
 from quintiles
-group by 1, 2, 3
+group by 1, 2
+union all
+select
+    frequency_quintile as quintile,
+    'frequency' as metric,
+    count(*) as users,
+    min(purchases) as minimum,
+    max(purchases) as maximum,
+    avg(purchases) as average
+from quintiles
+group by 1, 2
+union all
+select
+    monetary_quintile as quintile,
+    'monetary' as metric,
+    count(*) as users,
+    min(total_spend) as minimum,
+    max(total_spend) as maximum,
+    avg(total_spend) as average
+from quintiles
+group by 1, 2
