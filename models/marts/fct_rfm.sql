b82e963ada75b6488ce402584705cd48c5f88260
David Reynolds <dgazeyreyn@gmail.com>
2024-08-07 17:21:29 +0000

    adding median values for each quintile

--- models/marts/fct_rfm.sql
+++ models/marts/fct_rfm.sql
@@ -13,58 +13,93 @@ with
         select *, date_diff(latest_date.recency_date, last_order_date, day) as recency
         from latest_date
     ),
-    quintiles as (
+    ntiles as (
         select
             *,
             1+(rank() over (order by recency desc)-1) * 5 / count(1) over (partition by (select 1)) as recency_ntile,
             1+(rank() over (order by purchases)-1) * 5 / count(1) over (partition by (select 1)) as frequency_ntile,
             1+(rank() over (order by total_spend)-1) * 5 / count(1) over (partition by (select 1)) as monetary_ntile,
         from recency_days
+    ),
+    quintile_assignments as (
+        select
+            *,
+            case
+                when recency_ntile >= 1 and recency_ntile < 2
+                then 1
+                when recency_ntile >= 2 and recency_ntile < 3
+                then 2
+                when recency_ntile >= 3 and recency_ntile < 4
+                then 3
+                when recency_ntile >= 4 and recency_ntile < 5
+                then 4
+                when recency_ntile >= 5
+                then 5
+            end as recency_quintile,
+            case
+                when frequency_ntile >= 1 and frequency_ntile < 2
+                then 1
+                when frequency_ntile >= 2 and frequency_ntile < 3
+                then 2
+                when frequency_ntile >= 3 and frequency_ntile < 4
+                then 3
+                when frequency_ntile >= 4 and frequency_ntile < 5
+                then 4
+                when frequency_ntile >= 5
+                then 5
+            end as frequency_quintile,
+            case
+                when monetary_ntile >= 1 and monetary_ntile < 2
+                then 1
+                when monetary_ntile >= 2 and monetary_ntile < 3
+                then 2
+                when monetary_ntile >= 3 and monetary_ntile < 4
+                then 3
+                when monetary_ntile >= 4 and monetary_ntile < 5
+                then 4
+                when monetary_ntile >= 5
+                then 5
+            end as monetary_quintile
+        from ntiles
+    ),
+    medians as (
+    select
+        *,
+        PERCENTILE_CONT(recency, 0.5) OVER(PARTITION BY recency_quintile) AS recency_median,
+        PERCENTILE_CONT(purchases, 0.5) OVER(PARTITION BY frequency_quintile) AS frequency_median,
+        PERCENTILE_CONT(total_spend, 0.5) OVER(PARTITION BY monetary_quintile) AS monetary_median,
+    from
+        quintile_assignments
     )
 select
-    case
-        when recency_ntile >= 1 and recency_ntile < 2 then 1
-        when recency_ntile >= 2 and recency_ntile < 3 then 2
-        when recency_ntile >= 3 and recency_ntile < 4 then 3
-        when recency_ntile >= 4 and recency_ntile < 5 then 4
-        when recency_ntile >= 5 then 5
-    end as quintile,
+    recency_quintile,
     'recency' as metric,
     count(*) as users,
     min(recency) as minimum,
     max(recency) as maximum,
-    avg(recency) as average
-from quintiles
+    avg(recency) as average,
+    any_value(recency_median) as median
+from medians
 group by 1, 2
 union all
 select
-    case
-        when frequency_ntile >= 1 and frequency_ntile < 2 then 1
-        when frequency_ntile >= 2 and frequency_ntile < 3 then 2
-        when frequency_ntile >= 3 and frequency_ntile < 4 then 3
-        when frequency_ntile >= 4 and frequency_ntile < 5 then 4
-        when frequency_ntile >= 5 then 5
-    end as quintile,
+    frequency_quintile,
     'frequency' as metric,
     count(*) as users,
     min(purchases) as minimum,
     max(purchases) as maximum,
-    avg(purchases) as average
-from quintiles
+    avg(purchases) as average,
+    any_value(frequency_median) as median
+from medians
 group by 1, 2
 union all
 select
-    case
-        when monetary_ntile >= 1 and monetary_ntile < 2 then 1
-        when monetary_ntile >= 2 and monetary_ntile < 3 then 2
-        when monetary_ntile >= 3 and monetary_ntile < 4 then 3
-        when monetary_ntile >= 4 and monetary_ntile < 5 then 4
-        when monetary_ntile >= 5 then 5
-    end as quintile,
+    monetary_quintile,
     'monetary' as metric,
     count(*) as users,
     min(total_spend) as minimum,
     max(total_spend) as maximum,
-    avg(total_spend) as average
-from quintiles
+    avg(total_spend) as average,
+    any_value(monetary_median) as median
+from medians
 group by 1, 2
