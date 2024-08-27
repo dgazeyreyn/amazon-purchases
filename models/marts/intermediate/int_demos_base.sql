dfa0f341405921554cb4026afad25e2cae9a8143
David Reynolds <dgazeyreyn@gmail.com>
2024-08-27 17:02:29 +0000

    adding purchases and total_spend metrics to int_demos_base

--- models/marts/intermediate/int_demos_base.sql
+++ models/marts/intermediate/int_demos_base.sql
@@ -5,49 +5,63 @@ with
         select
             q_demos_age as demo_band,
             'age' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             case when q_demos_hispanic = true then 'yes' else 'no' end as demo_band,
             'hispanic' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             q_demos_race as demo_band,
             'race' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             q_demos_education as demo_band,
             'education' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             q_demos_income as demo_band,
             'income' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             q_demos_gender as demo_band,
             'gender' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
         union all
         select
             q_demos_state as demo_band,
             'state' as metric,
-            count(distinct survey_responseid) as users
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
         from purchases_responses
         group by 1, 2
     )
