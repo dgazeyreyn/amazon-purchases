43494df4cf97edefc105656a39f954feef5295a5
David Reynolds <dgazeyreyn@gmail.com>
2024-08-27 00:05:49 +0000

    lower casing field names and creating a total_spend calculation

--- models/staging/purchases/stg_purchases__amazon_purchases.sql
+++ models/staging/purchases/stg_purchases__amazon_purchases.sql
@@ -10,13 +10,21 @@ transformed as (
 
     select 
 
-        *,
+        order_date,
+        purchase_price_per_unit,
+        Quantity as quantity,
+        shipping_address_state,
+        Title as title,
+        asin_isbn_product_code,
+        Category as category,
+        survey_responseid,
         extract(year from order_date) as order_year,
         case
             when order_date in ('2018-07-16', '2018-07-17', '2019-07-15', '2019-07-16', '2020-10-13', '2020-10-14',
             '2021-06-21', '2021-06-22', '2022-07-12', '2022-07-13') then true
             else false
-        end as amazon_prime_day
+        end as amazon_prime_day,
+        purchase_price_per_unit * Quantity as total_spend
 
     from source
 
