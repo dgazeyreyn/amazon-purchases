9079edcaa8a7957f64f3bd865f24dd62c4134c92
David Reynolds <dgazeyreyn@gmail.com>
2024-08-28 17:18:23 +0000

    removing category grain and renaming model

---
+++ models/marts/fct_date.sql
@@ -0,0 +1,18 @@
+{{ config(materialized="table") }}
+with
+    purchases as (select * from {{ ref("stg_purchases__amazon_purchases") }}),
+
+    final as (
+
+        select
+            order_date,
+            amazon_prime_day,
+            count(distinct survey_responseid) as users,
+            count(*) as purchases,
+            sum(total_spend) as total_spend
+        from purchases
+        group by 1, 2
+
+    )
+select *
+from final
